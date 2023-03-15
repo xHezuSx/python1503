302bbd34da450a537f4761a0b2abe686a3c0a333
WWunn <wojciech.widzgowski@gmail.com>
2023-03-15 17:00:18 +0100

    commit 2

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import random
 import math
-#zadanie 1
+# zadanie 1
 # A=[1-x for x in range(1,10)]
 # B=[4**x for x in range(0,7)]
 # C=[B[x] for x in range(0,len(B)) if B[x]%2==0]
 # print(A)
 # print(B)
 # print(C)
-#zadanie 2
+# zadanie 2
 # listy=[]
 # for x in range(0,10):
 #     listy.append(random.randint(-400,100000))
 # implementacja=[listy[x] for x in range(0,10) if listy[x]%2==0]
 # print(listy)
 # print(implementacja)
-#zadanie 3
+# zadanie 3
 # wolnosc={}
-# liberty = {'kabanosy':'opakowanie', 'crossaint': 'sztuka', 'woda': 'butelka', 'sól': 'kilogram', 'cebula':'kilogram','majonez':'sztuka'}
+# liberty = {'crossaint': 'sztuka', 'woda': 'butelka', 'sól': 'kilogram', 'cebula':'kilogram','majonez':'sztuka'}
 # wolnosc=[x for x in liberty if liberty[x]=='sztuka']
 # print(liberty)
 # print(wolnosc)
-#zadanie 4
+# zadanie 4
 # def prostokatny(a,b,c):
 #     if (a*a)+(b*b)==(c*c):
 #         print('jest prostokatny')
@@ -34,7 +34,7 @@ import math
 # w=int(input())
 # d=int(input())
 # prostokatny(h, w, d)
-#zadanie 5
+# zadanie 5
 # def poletrapezu(podstawa=0, wysokosc=10, podstawa2=0):
 #     pole=(((podstawa+podstawa2)*wysokosc)/2)
 #     print(pole)
@@ -42,19 +42,19 @@ import math
 # b=int(input('b= '))
 # h=int(input('h= '))
 # poletrapezu(a,h,b)
-#zadanie 6
+# zadanie 6
 # def iloczyn(a=1,b=4,ile=10):
 #     koniec = a
 #     koniec *= b**(ile-1)
 #     return koniec
 # print(iloczyn())
-#zadanie 7
+# zadanie 7
 # def iloczyn(a=1,b=4,ile=10):
 #     koniec = a
 #     koniec *= b**(ile-1)
 #     return koniec
 # print(iloczyn())
-#zadanie 8
+# zadanie 8
 # def ilosc_wartosc(**lista):
 #     hajs = sum(lista.values())
 #     suma = len(lista)
@@ -64,7 +64,7 @@ import math
 # print('kosztuja ',hajs)
 # print('jest ich ',suma)
 
-#zadanie 9:
+# zadanie 9
 # x = int(input('podaj dodatnia wartosc= '))
 # try:
 #     sqrt = math.sqrt(x)
@@ -75,4 +75,3 @@ import math
 # print(sqrt)
 #
 # print(x)
-
